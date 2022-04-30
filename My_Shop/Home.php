6e2d7ba3159fd3e6ccd1c4e77bf09d61a9ad1172
Khan,Mansib  Ahsan <97165527+elusivemansiv@users.noreply.github.com>
2022-04-30 23:41:48 +0600

    Add files via upload

---
+++ My_Shop/Home.php
@@ -0,0 +1,271 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <title>Home</title>
+    
+
+
+</head>
+
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a  class="navbar-brand" href="#">BANDA SHOP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Products
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Shoes</a>
+          <a class="dropdown-item" href="#">Bags</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Watches</a>
+        </div>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="jumbotron">
+<h2 id="logo" class="display-5"> <b> GET YOUR PRODUCTS FROM BANDA SHOP NOW </b></h2>
+  <p id="para" class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse debitis sunt, consectetur.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+
+<h1 align="center" >BEST DEALS</h1>
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="img/1_496092_ZM_BLUE_ALT1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">SHOES</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      
+      Price <span id="price"> 50$</span><br><br>
+    <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/OIP.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">BAGS</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      
+      Price <span id="price"> 50$</span><br><br>
+    <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/R.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">HEADPHONES</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      
+      Price <span id="price"> 50$</span><br><br>
+    <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+
+
+    <hr>          
+    
+<!-- Default form contact -->
+<table align="center">
+  <td>
+
+    
+    <img src="img/contact_us_4x.webp" height="350px" width="350px" alt="">
+</td>
+
+
+<td>
+<form class="text-center border border-light p-5" action="#!">
+
+    <p class="h4 mb-4">Contact us</p>
+
+    <!-- Name -->
+    <input type="text" id="defaultContactFormName" class="form-control mb-4" placeholder="Name">
+
+    <!-- Email -->
+    <input type="email" id="defaultContactFormEmail" class="form-control mb-4" placeholder="E-mail">
+
+    
+
+    <!-- Message -->
+    <div class="form-group">
+        <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Message"></textarea>
+    </div>
+
+    <!-- Copy -->
+    <div class="custom-control custom-checkbox mb-4">
+        <input type="checkbox" class="custom-control-input" id="defaultContactFormCopy">
+        
+    </div>
+
+    <!-- Send button -->
+    <button class="btn btn-info btn-block" type="submit">Send</button>
+
+</form>
+
+</td>
+</table>
+<hr>
+
+<!-- Default form contact -->
+ 
+<!-- Footer -->
+<footer class="page-footer font-small indigo">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Very long link 1</a>
+          </li>
+          <li>
+            <a href="#!">Very long link 2</a>
+          </li>
+          <li>
+            <a href="#!">Very long link 3</a>
+          </li>
+          <li>
+            <a href="#!">Very long link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright by Ahsan Mansiv
+    <a href="/"></a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+ </section>
+
+ <script src="Home.js"></script>
+</body>
+</html>
